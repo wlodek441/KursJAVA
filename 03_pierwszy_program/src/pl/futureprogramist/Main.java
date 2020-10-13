1cba52f5db611dfff74fb71bfeb8cde81001cdae
wlodek441 <68141505+wlodek441@users.noreply.github.com>
2020-10-13 17:01:04 +0200

    Initial commit

---
+++ 03_pierwszy_program/src/pl/futureprogramist/Main.java
@@ -0,0 +1,11 @@
+package pl.futureprogramist;
+
+public class Main {
+    public static void main(String[] args) {
+        String message1 = "Piekna pogoda ";
+        String message2 = " brzydka pogoda ";
+        String message3 = " nienajlepsza pogoda ";
+
+        System.out.println(message1 + message2 + message3);
+    }
+}
